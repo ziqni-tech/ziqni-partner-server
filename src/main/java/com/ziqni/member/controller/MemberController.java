6c31027f04bd9755d8f6da4f8b8d07e0edfa4f85
mcdonavann <mcdonavann.mwale@ecocashholdings.co.zw>
2024-04-11 18:37:10 +0200

    generated files

--- src/main/java/com/ziqni/member/controller/MemberController.java
+++ src/main/java/com/ziqni/member/controller/MemberController.java
@@ -1,12 +1,6 @@
 package com.ziqni.member.controller;
 
 import com.ziqni.common.AbstractApiInvoker;
-import com.ziqni.common.Response;
-import com.ziqni.webhook.api.model.NewMember;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +10,14 @@ public class MemberController extends AbstractApiInvoker {
 
 
     public static final String ZIQNI_WEBHOOK_URL = "https://your.server.com/ziqni/webhook";
-
-    @PostMapping
-    public Response save(@RequestBody NewMember newMember) {
-        final var headers = getRequestHeaders();
-
-        final var requestEntity = new HttpEntity<>(newMember, headers);
-        return restTemplate.exchange(
-                ZIQNI_WEBHOOK_URL, HttpMethod.POST, requestEntity, Response.class).getBody();
-
-    }
+//
+//    @PostMapping
+//    public Response save(@RequestBody NewMember newMember) {
+//        final var headers = getRequestHeaders();
+//
+//        final var requestEntity = new HttpEntity<>(newMember, headers);
+//        return restTemplate.exchange(
+//                ZIQNI_WEBHOOK_URL, HttpMethod.POST, requestEntity, Response.class).getBody();
+//
+//    }
 }
