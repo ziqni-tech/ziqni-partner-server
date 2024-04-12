9c16e23fb68939e999feada1c65886d0f8d73eb9
vossie <carel.vosloo@bronzegate.com>
2024-04-12 16:44:22 +0100

    Changed to show how the request can be handled

--- src/main/java/com/ziqni/token/TokenController.java
+++ src/main/java/com/ziqni/token/TokenController.java
@@ -1,7 +1,7 @@
 package com.ziqni.token;
 
-import com.ziqni.admin.sdk.model.MemberTokenRequest;
 import com.ziqni.admin.sdk.model.TokenResponse;
+import com.ziqni.model.TokenRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,9 +17,9 @@ public class TokenController {
 
     private final TokenService tokenService;
     @PostMapping
-    public CompletableFuture<TokenResponse> getToken(@RequestBody MemberTokenRequest memberTokenRequest) {
+    public CompletableFuture<TokenResponse> getToken(@RequestBody TokenRequest tokenRequest) {
 
-        return tokenService.getToken(memberTokenRequest);
+        return tokenService.getToken(tokenRequest);
     }
 
 }
